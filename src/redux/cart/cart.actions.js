1cf7b3e2603c2abd6ffd3de8dbe5ba11db346580
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-04-04 12:48:02 +0200

    remove items from cart

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,10 +1,15 @@
 import CartActionTypes from "./cart.types";
 
 export const toggleCartHidden = () => ({
-  type: CartActionTypes.TOGGEL_CART_HIDDEN
+  type: CartActionTypes.TOGGEL_CART_HIDDEN,
 });
 
-export const addItem = item => ({
+export const addItem = (item) => ({
   type: CartActionTypes.ADD_ITEM,
-  payload: item
+  payload: item,
+});
+
+export const clearItemFormCart = (item) => ({
+  type: CartActionTypes.CLEAR_ITEM_FORM_CART,
+  payload: item,
 });
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,7 @@
 const CartActionTypes = {
   TOGGEL_CART_HIDDEN: "TOGGEL_CART_HIDDEN",
-  ADD_ITEM: "ADD_ITEM"
+  ADD_ITEM: "ADD_ITEM",
+  CLEAR_ITEM_FORM_CART: "CLEAR_ITEM_FORM_CART",
 };
 
 export default CartActionTypes;
