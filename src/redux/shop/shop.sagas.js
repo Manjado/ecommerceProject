137754926003cea5e329e27ed17432122a9209dc
malchimowicz <michal.alchimowicz@enovatis.pl>
2020-04-19 17:49:02 +0200

    add redux-saga

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,37 @@
+import { takeEvery, call, put } from "redux-saga/effects";
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
+
+import ShopActionTypes from "./shop.types";
+
+export function* fetchCollectionsAsync() {
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    // it equal to onvertCollectionsSnapshotToMap(snapshot)
+    // call give beter way to control or in case it needs to cancel
+    yield put(fetchCollectionsSuccess(collectionsMap));
+    // saga use put insted dispatch
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message));
+  }
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionsAsync
+  );
+}
